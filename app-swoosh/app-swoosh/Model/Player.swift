1280513905a2c7b857138f09c36f5219d108a204
Ali Sharkasi <alisharkas@gmail.com>
2021-07-24 22:17:16 +0300

    pass variable to VC

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Ali Sharkasi on 21/07/2021.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+}
